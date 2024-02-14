89a8fc896aae237e43d73fb58431ddcb91331089
entiku <entiku.dev@gmail.com>
2024-02-14 20:22:23 +0900

    サイコロを押すを何度もクリックしても回転数が変わらないように修正

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let clickCount = 0;
+
 document.getElementById('roll-button').addEventListener('click', function() {
-    var dice = document.getElementById('dice');
+    clickCount++;
+    
+    const dice = document.getElementById('dice');
 
     // X軸で5回以上回転させるための角度を計算（1800度は5回転）
-    var xRotation = 1800 + getRandomInt(0, 5) * 90; // X軸の追加回転を加える
+    const xRotation = 1800 * clickCount + getRandomInt(0, 5) * 90; // X軸の追加回転を加える
 
     // Y軸は常に同じ角度（0度、90度、180度、270度）のいずれかで停止させ、サイコロが正面を向くようにする
-    var yRotation = getRandomInt(0, 3) * 90; // Y軸の回転を設定
+    const yRotation = 180 * clickCount + getRandomInt(0, 3) * 90; // Y軸の回転を設定
 
     dice.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
 });
